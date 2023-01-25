7ef8c3fff16b06457f631f828cab6282d8f5ca19
KishanLimbad0611 <kishanlimbad061101@gmail.com>
2023-01-26 00:30:36 +0530

    ACM

---
+++ ACM/UnitTestProject1/customerTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Remoting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //----Arrange 
+            Customer customer = new customer;
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+
+            string expected = "Baggins, Bilbo";
+            
+            //----Act 
+            string actual = customer.FullName;
+
+            //----Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
+
+
+
