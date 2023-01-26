21f5a6749dcf7d1764dc96d6da19c5ad575a0788
KishanLimbad0611 <kishanlimbad061101@gmail.com>
2023-01-26 14:14:10 +0530

    Basic Tutorial

---
+++ tutorials of c Sharp/class and object Basics/class and object Basics/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_and_object_Basics
+{
+    internal class Book
+    {
+        public string title;
+        public string description;
+        public int pages;
+    }
+}
---
+++ tutorials of c Sharp/class and object Basics/class and object Basics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_and_object_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1= new Book();
+            book1.title = "Mr name";
+            book1.description = "hello";
+            book1.pages = 700;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.description);
+            Console.WriteLine(book1.pages);
+            Console.ReadLine();
+        }
+       
+
+    }
+}
