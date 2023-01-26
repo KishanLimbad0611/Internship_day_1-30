d823e470feece555dae687cd8667e3c84e76d992
KishanLimbad0611 <kishanlimbad061101@gmail.com>
2023-01-26 10:29:45 +0530

    Solid Principles of c#

---
+++ Solid Principles of C#/DIPDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class CustomerDataAccess : ICustomerDataAccess
+    {
+        public CustomerDataAccess()
+        {
+
+        }
+
+        public string GetCustomerNameDataAccess()
+        {
+            return "Customer Name";
+        }
+    }
+
+    public interface ICustomerDataAccess
+    {
+        string GetCustomerNameDataAccess();
+    }
+
+    public class DataAccessFactory
+    {
+        public static ICustomerDataAccess GetCustomerName()
+        {
+            return new CustomerDataAccess(); 
+        }
+    }
+    public class CustomerBL
+    {
+        public CustomerBL()
+        {
+
+        }
+        public void GetCustomerNameBL()
+        {
+            ICustomerDataAccess obj = DataAccessFactory.GetCustomerName();
+            obj.GetCustomerNameDataAccess();
+        }
+    }
+}
