007821d3d3da0dbbd94e80cc3ee359b4083634f3
KishanLimbad0611 <kishanlimbad061101@gmail.com>
2023-01-26 10:48:18 +0530

    solid Principles in C#

---
+++ Solid Principles of C#/LIPDemo/LIPDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIPDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new PartTimeEmployee();
+        }
+    }
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class PartTimeEmployee : Employee
+    {
+        public decimal DailyWage { get; set; }
+    }
+
+    class FullTimeEmployee : Employee
+    {
+        public decimal HourlyWage { get; set; }
+    }
+}
