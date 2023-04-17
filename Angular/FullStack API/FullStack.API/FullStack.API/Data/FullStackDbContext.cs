90f748f125f7c702d403db6170e7a09c3ccf36e3
KishanLimbad0611 <96331925+KishanLimbad0611@users.noreply.github.com>
2023-04-17 17:34:24 +0530

    Crud Operation

---
+++ Angular/FullStack API/FullStack.API/FullStack.API/Data/FullStackDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FullStack.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        //-----------created the constructor 
+        public FullStackDbContext(DbContextOptions options) : base(options) { 
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Angular/FullStack API/newwone/newwone/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using newwone.Model;
+
+namespace newwone.Data
+{
+    public class FullStackDbContext :DbContext
+    {
+        //-----------created the constructor 
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
